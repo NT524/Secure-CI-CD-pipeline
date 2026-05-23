60ec94389a8dac39ca890fbed05df3b89c387381
Truong <truongscp@gmail.com>
2026-05-23 16:46:09 +0700

    Feat: Complete fully automated Keyless and SBOM Attestation gating pipeline

--- terraform/Namespace.tf
+++ terraform/Namespace.tf
@@ -2,10 +2,13 @@
 resource "kubernetes_namespace" "nodegoat_staging" {
   metadata {
     name = var.namespace
-    labels ={
+    labels = {
         environment = "staging"
         app         = "nodegoat"
         "pod-security.kubernetes.io/enforce" = "baseline"
+        
+        # DÒNG NÀY ĐỂ KÍCH HOẠT CHỐT CHẶN KEYLESS POLICY CONTROLLER:
+        "policy.sigstore.dev/include"        = "true"
     }
   }
 }
