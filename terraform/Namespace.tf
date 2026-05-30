18eac1887b329ab32241252005c7f18e4b944a71
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-30 15:59:24 +0700

    Remove Terraform refresh state step from deployment workflow

--- terraform/Namespace.tf
+++ terraform/Namespace.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_namespace" "isolated_namespace" {
-  depends_on = [module.eks, module.vpc] # Chỉ tạo sau khi cụm EKS và VPC đã dựng xong thành công
+  depends_on = [module.eks] 
 
   metadata {
     name = var.K8S_NAMESPACE
